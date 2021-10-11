f8ea5c08a56f548c1b44cd44f8f61f110affcd9a
StasTikh <tmihatelefan@gmail.com>
2021-10-11 13:13:16 +0300

    code improvements

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,23 +34,7 @@ function reverseSum() {
 
     if (sum >= 100 && sum <= 999) {
         if(checkSum[checkSum.length-1] != '0'){
-            if(sum >= 111 && sum <= 119) {
-                finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
-            }else if(sum >= 211 && sum <= 219) {
-                finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
-            }else if(sum >= 311 && sum <= 319) {
-                finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
-            }else if(sum >= 411 && sum <= 419) {
-                finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
-            }else if(sum >= 511 && sum <= 519) {
-                finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
-            }else if(sum >= 611 && sum <= 619) {
-                finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
-            }else if(sum >= 711 && sum <= 719) {             
-                finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
-            }else if(sum >= 811 && sum <= 819) {             
-                finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
-            }else if(sum >= 911 && sum <= 919) {             
+            if(checkSum[1] == '1') {
                 finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
             } else {             
                 finalSum = hundreds [checkSum[0]] + ' ' + twentys[checkSum[1]] + ' ' + tens[checkSum[2]];
@@ -68,7 +52,7 @@ function reverseSum() {
         }
     } else if (checkSum[checkSum.length-1] == 2 || checkSum[checkSum.length-1] == 3 || checkSum[checkSum.length-1] == 4) {
         if(checkSum[checkSum.length-2] == 1){
-            outputCode.innerHTML = `${finalSum.toUppercase()} (Гривен)`;
+            outputCode.innerHTML = `${finalSum} (Гривен)`;
         } else {
             outputCode.innerHTML = `${finalSum} (Гривны)`;
         }
@@ -76,3 +60,20 @@ function reverseSum() {
         outputCode.innerHTML = `${finalSum} (Гривен)`;
     }   
 }
+
+// }else if(sum >= 211 && sum <= 219) {
+            //     finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
+            // }else if(sum >= 311 && sum <= 319) {
+            //     finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
+            // }else if(sum >= 411 && sum <= 419) {
+            //     finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
+            // }else if(sum >= 511 && sum <= 519) {
+            //     finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
+            // }else if(sum >= 611 && sum <= 619) {
+            //     finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
+            // }else if(sum >= 711 && sum <= 719) {             
+            //     finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
+            // }else if(sum >= 811 && sum <= 819) {             
+            //     finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
+            // }else if(sum >= 911 && sum <= 919) {             
+            //     finalSum = hundreds [checkSum[0]] + ' ' + teens[checkSum[2]];
